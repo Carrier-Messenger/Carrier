85d3a75d7c2d0ff16c2511cac7b0087cbfc5e1ce
ciomeczek <ciomek05@gmail.com>
2021-11-15 16:18:08 +0100

    Chatroom serializers change
    -Added some fields
    -Added unique ChatroomUserSerializer

--- friend/serializer.py
+++ friend/serializer.py
@@ -5,6 +5,7 @@ from .models import FriendList, FriendRequest
 
 class FriendSerializer(serializers.ModelSerializer):
     full_name = serializers.ReadOnlyField()
+    friend_type = serializers.SerializerMethodField()
 
     class Meta:
         model = get_user_model()
@@ -13,7 +14,27 @@ class FriendSerializer(serializers.ModelSerializer):
                   'first_name',
                   'last_name',
                   'full_name',
-                  'pfp']
+                  'pfp',
+                  'friends',
+                  'friend_type']
+
+    def get_friends(self, user):
+        query_set = FriendList.objects.get(owner=user)
+        return FriendListSerializer(query_set, context={'request': self.context.get('request')}).data.get('friends')
+
+    def get_friend_type(self, user):
+        request_user = self.context.get('request').user
+        if not request_user.is_authenticated:
+            return "none"
+
+        if FriendRequest.objects.filter(sender=user, receiver=request_user).exists():
+            return "requested"
+        elif FriendRequest.objects.filter(sender=request_user, receiver=user).exists():
+            return "invited"
+        elif request_user.friend_list.is_friend(user):
+            return "friend"
+        else:
+            return "none"
 
 
 class FriendListSerializer(serializers.ModelSerializer):
